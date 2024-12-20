823f0d7e6123429a3fbe6d28a0ad978f3502db87
LuisFelipeWanumen <61471916+LuisFelipeWanumen@users.noreply.github.com>
2024-12-20 05:17:28 -0500

    Delete tabla_09_Modulo_01 directory

--- tabla_09_Modulo_01/script_menu.js
+++
@@ -1,8 +0,0 @@
-// Obtener los elementos del DOM
-const menuBtn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
-
-// Función para alternar la visibilidad del menú
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('show');
-});
